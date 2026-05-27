db64f186ffd4e3d4e9f7771bf92f883f27b023c6
pro <vp632728@gmail.com>
2026-05-27 17:23:22 +0300

    add Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import styles from './page.module.css';
 import Header from '@/components/Header/Header';
+import Hero from '@/components/Hero/Hero';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
   );
 }
