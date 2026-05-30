42ecfbfb1844c717ee3fd1aba9d5450eea778195
pro <vp632728@gmail.com>
2026-05-30 21:49:09 +0300

    update Footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import { Field, Form, Formik } from 'formik';
 import { useId } from 'react';
 import styles from './Footer.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   const emailId = useId();
@@ -11,15 +12,20 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footer_container}>
         <div className={styles.main_container}>
-          <div className={styles.icon_circle}>
-            <svg className={styles.icon} width="32" height="32">
-              <use href="/image/icons.svg#icon-boat_railway"></use>
-            </svg>
-          </div>
-          <h2 className={styles.title}>Keep up with the latest</h2>
+          <Link href="/">
+            <Image
+              className={styles.image}
+              src="/image/mini-logo.png"
+              alt="ТОВ Дім Дороша"
+              width="52"
+              height="52"
+            ></Image>
+          </Link>
+          <h2 className={styles.title}>ТОВ «Дім Дороша»</h2>
           <p className={styles.description}>
-            Fast.reable,and cost-effectie logistics solutions tailored to your
-            business-from local delveries to global freight forwarding
+            Організовуємо вантажні перевезення під задачі вашого бізнесу:
+            підбираємо транспорт, координуємо перевізників, супроводжуємо
+            доставку та контролюємо кожен етап маршруту.
           </p>
         </div>
         <div className={styles.form_container}>
@@ -27,7 +33,9 @@ export default function Footer() {
             {({ errors, touched }) => (
               <Form className={styles.form}>
                 <label className={styles.form_label} htmlFor={emailId}>
-                  <span className={styles.field_name}>Email</span>
+                  <span className={styles.field_name}>
+                    Підписка на оновлення
+                  </span>
                   <Field
                     className={styles.field}
                     type="email"
@@ -40,24 +48,47 @@ export default function Footer() {
                   )}
                 </label>
                 <button className={styles.button} type="button">
-                  Submit
+                  Підписатися
                 </button>
               </Form>
             )}
           </Formik>
-          <p className={styles.form_description}>more description here</p>
+          <p className={styles.form_description}>
+            Отримуйте корисні оновлення про логістику, перевезення та роботу
+            компанії.
+          </p>
         </div>
       </div>
-      <p className={styles.credits}>
-        Design based on a Figma Community template by{' '}
-        <Link
-          className={styles.credits_link}
-          href="https://www.figma.com/@duozhuamiao"
-        >
-          @duozhuamiao
-        </Link>
-        , modified by Vladyslav Pavlenko.
-      </p>
+
+      <div className={styles.credits}>
+        <ul className={styles.credits_list}>
+          <li className={styles.credits_list_item}>
+            <p className={styles.credits_list_copyright}>
+              &copy; 2026 ТОВ «Дім Дороша». Усі права захищено.
+            </p>
+          </li>
+          <li className={styles.credits_list_item}>
+            <Link className={styles.credits_list_link} href="">
+              Політика конфіденційності
+            </Link>
+          </li>
+          <li className={styles.credits_list_item}>
+            <Link className={styles.credits_list_link} href="">
+              Умови використання
+            </Link>
+          </li>
+        </ul>
+        <p className={styles.credits_figma}>
+          Design based on a Figma Community template by{' '}
+          <Link
+            className={styles.credits_link}
+            href="https://www.figma.com/@duozhuamiao"
+          >
+            @duozhuamiao
+          </Link>
+          , modified by Vladyslav Pavlenko.
+        </p>
+      </div>
     </footer>
   );
 }
